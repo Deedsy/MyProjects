52e9b8c9b9b583396dda61b3a45f47e5615bd96c
Deedsy <rhsribeiro@gmail.com>
2021-02-26 22:04:03 +0000

    commit 'master'

---
+++ BlockChainExample/src/main/java/com/deedsy/BlockChainProgram.java
@@ -0,0 +1,14 @@
+package com.deedsy;
+
+/**
+ * Hello world!
+ *
+ */
+public class BlockChainProgram 
+{
+    public static void main( String[] args )
+    {
+        String aString = "Ricardo";
+        System.out.print(aString.hashCode());;
+    }
+}
